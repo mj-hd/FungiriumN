400af24d910190073d534c8cc0754561c5f339b6
mjhd <mjhd@devlion.net>
2014-11-12 14:26:12 +0900

    サンプルコードを削除

--- FungiriumN/TestTubeScene.cs
+++ FungiriumN/TestTubeScene.cs
@@ -11,37 +11,18 @@ namespace FungiriumN
 	{
 		public TestTubeScene (SizeF size) : base (size)
 		{
-			// Setup your scene here
-			BackgroundColor = new UIColor (0.15f, 0.15f, 0.3f, 1.0f);
+			BackgroundColor = new UIColor (1.0f, 1.0f, 1.0f, 1.0f);
 
-			var myLabel = new SKLabelNode ("Chalkduster") {
-				Text = "Hello, World!",
-				FontSize = 30,
+			// 試験管を追加
+			var testTube = new Sprites.TestTubeSprite () {
+				Position = new PointF (Frame.X + Frame.Width / 2, Frame.Y + Frame.Height / 2),
 			};
 
-			myLabel.Position = new PointF (Frame.X + Frame.Width / 2, Frame.Y + Frame.Height / 2);
-
-			AddChild (myLabel);
-		}
-
-		public override void TouchesBegan (NSSet touches, UIEvent evt)
-		{
-			// Called when a touch begins
-			foreach (var touch in touches) {
-				var location = ((UITouch)touch).LocationInNode (this);
-				var sprite = new SKSpriteNode ("Spaceship");
-				sprite.Position = location;
-
-				var action = SKAction.RotateByAngle ((float)Math.PI, 1.0);
-				sprite.RunAction (SKAction.RepeatActionForever (action));
-
-				AddChild (sprite);
-			}
+			AddChild (testTube);
 		}
 
 		public override void Update (double currentTime)
 		{
-			// Run before each frame is rendered
 			base.Update (currentTime);
 		}
 	}
