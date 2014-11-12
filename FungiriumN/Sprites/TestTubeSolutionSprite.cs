af501d016ead7d8a752ec913cbae3c16ab600fa6
mjhd <mjhd@devlion.net>
2014-11-12 14:23:20 +0900

    試験管関係のクラスを追加

---
+++ FungiriumN/Sprites/TestTubeSolutionSprite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using MonoTouch.SpriteKit;
+
+namespace FungiriumN.Sprites
+{
+	public class TestTubeSolutionSprite : SKSpriteNode
+	{
+		public TestTubeSolutionSprite ()
+			: base()
+		{
+			var texture = SKTexture.FromImageNamed ("TestTubeSolution.png");
+
+			this.Texture = texture;
+			this.NormalTexture = texture;
+			this.Size = texture.Size;
+		}
+	}
+}
+
---
+++ FungiriumN/Sprites/TestTubeSprite.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using MonoTouch.SpriteKit;
+
+namespace FungiriumN.Sprites
+{
+	public class TestTubeSprite : SKSpriteNode
+	{
+		public TestTubeSprite ()
+			: base()
+		{
+			var texture = SKTexture.FromImageNamed ("TestTube.png");
+
+			this.Texture = texture;
+			this.NormalTexture = texture;
+
+			this.Size = texture.Size;
+
+
+			// 溶液を追加	
+			var solution = new TestTubeSolutionSprite ();
+			this.AddChild (solution);
+		}
+	}
+}
+
