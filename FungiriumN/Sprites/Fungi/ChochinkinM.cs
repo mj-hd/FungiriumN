1889aa5aede1a1c1b96e462684f0cdc372a3e45b
mjhd <mjhd@devlion.net>
2014-11-14 17:43:34 +0900

    Chochinkinの名前が誤っていたのを修正

---
+++ FungiriumN/Sprites/Fungi/ChochinkinM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FungiriumN.Sprites.Fungi
+{
+	public class ChochinkinM : SampleFungus, IFungus
+	{
+		static string InternalName = "Chochinkin_m";
+
+		public ChochinkinM ()
+			: base()
+		{
+		}
+
+		protected override string _InternalName { get { return ChochinkinM.InternalName; }}
+ 	}
+}
+
