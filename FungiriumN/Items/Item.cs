91b6cae1c0fe0ab5ca86f76c1de395b56eeb75c3
mjhd <mjhd@devlion.net>
2014-11-21 18:22:54 +0900

    サンプルアイテムを作成。

--- FungiriumN/Items/Item.cs
+++ FungiriumN/Items/Item.cs
@@ -2,14 +2,40 @@
 
 namespace FungiriumN.Items
 {
-	public class Item
+	public class Item : ICloneable
 	{
+		public static Metadata Metadata = new Metadata () {
+			Name = "さんぷるあいてむ",
+			InternalName = "SampleItem",
+			Description = "これはサンプルのアイテムです。",
+			Price = 100
+		};
+
+		#region ICloneable
+
+		object ICloneable.Clone()
+		{
+			return this.Clone ();
+		}
+
+		public virtual Item Clone()
+		{
+			Item instance = (Item)Activator.CreateInstance (this.GetType ());
+
+			// TODO: コピー処理
+
+			return instance;
+		}
+
+		#endregion
+
 		public Item ()
 		{
 		}
 
 		public virtual bool UseToTestTube (Sprites.TestTubeSprite testTube)
 		{
+			// TODO: 使用処理
 
 			return true;
 		}
